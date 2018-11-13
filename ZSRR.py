640fd3f22d2c3d99014ad3c50ebab79f650ae38c
“jakis <wanwnofnkawnvoanb@gmail.com>
2018-11-13 17:52:17 +0100

    Bois-we-have-a-traitor

---
+++ ZSRR.py
@@ -0,0 +1,26 @@
+Stalin = float(2)
+i = 2
+while i < 25:
+  print(i)
+  i += 2;
+  lista = [14,"owca","owca2","19",24,18,29,84,9019,True,False,
+		 [1,2,3,4,5],"ojoj jak dużo elemętów ma ta lista","co zorbić"]
+print (lista[7])
+lista2 = lista[4:10]
+print(lista2)
+print(len(lista))
+
+input()
+
+tup =(1,2,3)
+print('W TUPIE NIE DA SIE ZMIENIAC WARTOSCI')
+
+input()
+
+slow = {'jeden'10,'dwa'20}
+print(slow['jeden'])
+
+input()
+
+slow['trzy']=30
+print(slow)
